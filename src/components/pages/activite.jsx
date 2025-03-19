b13073c632a23f9283ba266b296e69ca944dc2c5
salahelfatimi <salahfatimi16@gmail.com>
2025-03-19 15:28:30 +0000

    Increase paragraph font size in Activitte component for improved readability

--- src/components/pages/activite.jsx
+++ src/components/pages/activite.jsx
@@ -12,7 +12,7 @@ export default function Activitte() {
                     <h2 className=" text-4xl text-center font-boska font-medium mb-10">
                         Adventures, Culture & Pure Fun
                     </h2>
-                    <p className={`text-sm lg:text-lg text-center font-boska mb-4`}>
+                    <p className={`text-sm lg:text-xl text-center  mb-4`}>
                         From sunrise bike rides through palm groves to sunset yoga on the terrace, Caravan Serai turns every moment into an adventure. Explore local art, dive into Moroccan culture, or challenge your friends to a game of billiards — because at Caravan Serai, every day is a new story waiting to be written.
                     </p>
                     <button className="bg-white hover:bg-primary border-2 border-white hover:text-white duration-700 text-primary text-2xl font-boska px-8 py-2">
