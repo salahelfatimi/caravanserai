8bc3916534c22b54e0142eb35b9a0b82c71ea61d
salahelfatimi <salahfatimi16@gmail.com>
2025-03-20 15:29:28 +0000

    Add loading and not found components, and configure sitemap and robots.txt

---
+++ src/app/loading.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Loading() {
+    return (
+      <>
+        <div className={`fixed  inset-0 flex items-center justify-center bg-white  duration-1000 transition z-[70] `}>
+            <Image src="/img/logo_single.png" height={500} width={500} alt="Caravan Serai" title="Caravan Serai" className="w-48 animate-bounce " />
+            <span className=" text-primary flex font-boska  items-center justify-center  font-extrabold  gap-3 md:text-5xl text-4xl ">
+              caravanserai
+            </span>
+        </div>
+      </>
+    );
+  }
