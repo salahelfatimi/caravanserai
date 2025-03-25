48e0e1762a32fc313380e199198adecbeacb01a2
salahelfatimi <salahfatimi16@gmail.com>
2025-03-25 14:13:46 +0000

    Refactor carousel configuration in Suites_Page and remove unused button component

--- src/components/pages/suites_page.jsx
+++ src/components/pages/suites_page.jsx
@@ -9,7 +9,7 @@ import suites from '../../app/data/suites';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 
 export default function Suites_Page() {
-    const [emblaRef] = useEmblaCarousel({ loop: true, align: 'start', containScroll: 'trimSnaps', dragFree: true, draggable: false },
+    const [emblaRef] = useEmblaCarousel({ loop: true, align: 'start' },
         [AutoScroll({ playOnInit: true, speed: 1, stopOnInteraction: false, stopOnMouseEnter: true, stopOnFocusIn: false })])
     return (
         <div className="flex flex-col gap-6 py-20 bg-contain lg:bg-[url(/img/bg-yellow.svg)]">
@@ -28,11 +28,6 @@ export default function Suites_Page() {
                     ))}
                 </div>
             </div>
-            {/* <div className=' w-full  flex items-center justify-center'>
-                <button className={` bg-primary rounded-2xl hover:bg-white border-2 w-fit border-primary hover:text-primary duration-700 text-white text-xl font-medium  px-8 py-2`}>
-                    See All Suites
-                </button>
-            </div> */}
         </div>
     );
 }
