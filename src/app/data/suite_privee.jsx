600f1f81336c0903889122fd2c2b185b17f10de4
salahelfatimi <salahfatimi16@gmail.com>
2025-03-25 14:52:47 +0000

    Fix image array syntax in suite_privee data for consistency

--- src/app/data/suite_privee.jsx
+++ src/app/data/suite_privee.jsx
@@ -7,7 +7,7 @@ const suite_privee =[
     {
         id: 'suites',
         title: 'Suites',
-        images: ['/img/suites&rooms/suites_10/suites_1.jpg','/img/suites&rooms/suites_10/suites_2.jpg','/img/suites&rooms/suites_10/suites_3.jpg','/img/suites&rooms/suites_10/suites_4.jpg',,'/img/suites&rooms/suites_10/suites_5.jpg',,'/img/suites&rooms/suites_10/suites_6.jpg',,'/img/suites&rooms/suites_10/suites_7.jpg',,'/img/suites&rooms/suites_10/suites_8.jpg',,'/img/suites&rooms/suites_10/suites_9.jpg',,'/img/suites&rooms/suites_10/suites_10.jpg']
+        images: ['/img/suites&rooms/suites_10/suites_1.jpg','/img/suites&rooms/suites_10/suites_2.jpg','/img/suites&rooms/suites_10/suites_3.jpg','/img/suites&rooms/suites_10/suites_4.jpg','/img/suites&rooms/suites_10/suites_5.jpg','/img/suites&rooms/suites_10/suites_6.jpg','/img/suites&rooms/suites_10/suites_7.jpg','/img/suites&rooms/suites_10/suites_8.jpg','/img/suites&rooms/suites_10/suites_9.jpg','/img/suites&rooms/suites_10/suites_10.jpg']
     },
 ]
 
