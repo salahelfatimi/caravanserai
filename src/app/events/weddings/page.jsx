d561fdd5ea80d1b3b07efb205ab03b8d2e68fb65
salahelfatimi <salahfatimi16@gmail.com>
2025-03-27 11:02:11 +0000

    Replace wedding images with background videos for enhanced visual experience

--- src/app/events/weddings/page.jsx
+++ src/app/events/weddings/page.jsx
@@ -46,10 +46,13 @@ export default function Weddings(){
                <p className=" container text-center text-white font-medium text-xs lg:text-base">At Caravan Serai, we offer the perfect backdrop for your dream wedding or special celebration. Nestled in the heart of Marrakech, our enchanting venue blends Moroccan elegance with modern luxury, creating an unforgettable experience for you and your guests.</p>
             </div>
             <div className=" flex lg:flex-row-reverse flex-col items-center justify-center gap-3  bg-top bg-[url(/img/bg-yellow.svg)] ">
-                <div className="lg:w-1/2 h-screen">
-                    <Image src={'/img/events/weddings_and_celebrations/weddings_and_celebrations_2.jpg'} width={1920} height={1080} className="h-full  w-full object-cover" alt="Caravan Serai" title="Caravan Serai"/>
+                <div className=" h-screen">
+                    <video autoPlay loop muted className="w-full h-full  object-cover " about="Caravan Serai" title="Caravan Serai" > 
+                        <source src="/vid/events/weddings/weddings_1.mp4" type="video/mp4"/>
+                        Your browser does not support the video tag.
+                    </video>
                 </div>
-                <div className=" lg:w-1/2 p-4 flex flex-col gap-4">
+                <div className=" p-4 flex flex-col gap-4">
                     <h2 className="font-boska font-medium text-2xl lg:text-4xl text-primary">Why Choose Caravan Serai?</h2>
                     <div className="flex flex-col gap-4">
                         <ul className=" list-disc list-inside text-sm space-y-2">
@@ -70,6 +73,12 @@ export default function Weddings(){
                         </ul>
                     </div>
                     <Link href="/contact" className=" text-center hover:bg-white hover:text-primary border-primary border duration-700 bg-primary w-full p-2 px-3 text-2xl font-medium text-white rounded-full">Book Your Celebration</Link>
+                </div>
+                <div className=" h-screen">
+                    <video autoPlay loop muted className="w-full h-full  object-cover " about="Caravan Serai" title="Caravan Serai" > 
+                        <source src="/vid/events/weddings/weddings.mp4" type="video/mp4"/>
+                        Your browser does not support the video tag.
+                    </video>
                 </div>
             </div>
         </div>
