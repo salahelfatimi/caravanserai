d2df13df76a42da913312bfddcd93304a9059c5e
salahelfatimi <salahfatimi16@gmail.com>
2025-04-02 11:26:53 +0000

    Add link to Trafic Genius in footer for attribution

--- src/components/pages/footer/footer.jsx
+++ src/components/pages/footer/footer.jsx
@@ -1,6 +1,7 @@
 'use client'
 import { Instagram } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer(){
     return(
@@ -17,7 +18,7 @@ export default function Footer(){
             <div className=" bg-black py-6 ">
                 <div className=" container flex flex-col-reverse gap-6 lg:flex-row items-center justify-between ">
                     <div >
-                        <p className={`select-none flex  flex-col lg:flex-row gap-1  font-boska  items-center font-semibold capitalize text-center lg:text-start text-white `}> Copyright &copy; {new Date().getFullYear()} . Touts les droits sont réservés <span className=" text-primary   "> caravan serai </span> </p>
+                        <p className={`select-none flex  flex-col lg:flex-row gap-1   items-center font- capitalize text-center lg:text-start text-white `}> Copyright &copy; {new Date().getFullYear()} . Touts les droits sont réservés <span className=" text-primary   "> caravan serai </span> made by <Link href={'https://www.traficgenius.com/'} target="_blank" rel="noopener noreferrer" className=" underline underline-offset-4">trafic genius </Link>  </p>
                     </div>
                     <div className="flex gap-6">
                         <Instagram size={30} className=" stroke-[#fff]" />
@@ -25,6 +26,5 @@ export default function Footer(){
                 </div>
             </div>
         </div>
-       
     )
 }
